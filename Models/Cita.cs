2b35e1957b4146cd06e410860ef35bea20dd4325
katto-code <kattojaram@gmail.com>
2026-03-03 05:55:38 -0600

    Update Cita.cs

--- Models/Cita.cs
+++ Models/Cita.cs
@@ -1,18 +1,24 @@
-public class Cita
-{
-    [Key]
-    public int id_cita { get; set; }
-    public int id_expediente { get; set; }
-    public int id_empresa { get; set; }
-    public int id_usuario_tramito { get; set; }
-    
-    // IDs de relación
-    public int? id_servicio { get; set; }
-    public int? id_prestador { get; set; }
+using System.ComponentModel.DataAnnotations; 
 
-    public DateTime fecha { get; set; }
-    public TimeSpan hora { get; set; }
-    
-    public decimal? precio_final { get; set; }
-    public string estatus { get; set; } = "pendiente";
+namespace axcan.Models
+{
+    public class Cita
+    {
+        [Key] // Ahora sí va a reconocer esto
+        public int id_cita { get; set; }
+        
+        public int id_expediente { get; set; }
+        public int id_empresa { get; set; }
+        public int id_usuario_tramito { get; set; }
+        
+        public int? id_servicio { get; set; }
+        public int? id_prestador { get; set; }
+
+        public DateTime fecha { get; set; }
+        public TimeSpan hora { get; set; }
+        
+        public decimal? precio_final { get; set; }
+        public string estatus { get; set; } = "pendiente";
+        public string? notas_adicionales { get; set; }
+    }
 }
