6773a1efa62e45f494a3e8f3a860a2bf983c0f0c
katto-code <kattojaram@gmail.com>
2026-03-04 09:11:25 -0600

    Update Secretario.cs

--- Models/Secretario.cs
+++ Models/Secretario.cs
@@ -8,15 +8,8 @@ namespace axcan.Models
     {
         [Key]
         public int id_secretario { get; set; }
-        
         public int id_usuario { get; set; }
-        public int id_empresa { get; set; }
-        
-        // Aquí es donde vive el 'prestador' o 'secretario'
-        public string subrol { get; set; } = "secretario";
-
-        // Relaciones (Opcional para facilitar consultas)
-        [ForeignKey("id_usuario")]
-        public virtual Usuario? Usuario { get; set; }
+        public int id_empresa { get; set; } 
+        public string subrol { get; set; }
     }
 }
